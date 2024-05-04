67d529b47f9ff93f371a4c88274eac0b698f3d8f
lamngocchien <lamngocchien@gmail.com>
2024-05-05 06:17:31 +0700

    Create pyinstaller.py

---
+++ pyinstaller.py
@@ -0,0 +1,47 @@
+# -*- mode: python -*-
+# -*- coding: utf-8 -*-
+
+"""
+This is a PyInstaller spec file.
+"""
+
+import os
+from PyInstaller.building.api import PYZ, EXE, COLLECT
+from PyInstaller.building.build_main import Analysis
+from PyInstaller.utils.hooks import is_module_satisfies
+
+# Constants
+DEBUG = os.environ.get("CEFPYTHON_PYINSTALLER_DEBUG", False)
+
+# ----------------------------------------------------------------------------
+# Main
+# ----------------------------------------------------------------------------
+a = Analysis(
+    ["./wxpython.py"],
+    hookspath=["."],  # To find "hook-cefpython3.py"
+)
+
+if not os.environ.get("PYINSTALLER_CEFPYTHON3_HOOK_SUCCEEDED", None):
+    raise SystemExit("Error: Pyinstaller hook-cefpython3.py script was "
+                     "not executed or it failed")
+
+pyz = PYZ(a.pure,
+          a.zipped_data)
+
+exe = EXE(pyz,
+          a.scripts,
+          exclude_binaries=True,
+          name="cefapp",
+          debug=DEBUG,
+          strip=False,
+          upx=False,
+          console=DEBUG,
+          icon="../resources/wxpython.ico")
+
+COLLECT(exe,
+        a.binaries,
+        a.zipfiles,
+        a.datas,
+        strip=False,
+        upx=False,
+        name="cefapp")
